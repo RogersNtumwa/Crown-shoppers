25ddf3affbce6e70bda3aae76e061595f9944220
Rogers Ntumwa <ntumwar@gmail.com>
2020-06-23 00:55:05 +0400

    added assets folder to hold out crown svg and also added the header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Homepage from "./pages/homepage/Homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" component={Homepage} exact />
         <Route path="/shop" component={ShopPage} exact />
