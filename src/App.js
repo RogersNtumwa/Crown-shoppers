9a0b1a890c4afd5e6955fc2db03fef5a123af0f3
Rogers Ntumwa <ntumwar@gmail.com>
2020-06-27 00:15:20 +0400

    Another commit for sign in and sign up form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={Homepage} exact />
-        <Route path="/shop" component={ShopPage} exact />
-        <Route path="/signIn" component={SignInAndSignUpPage} exact />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+  unSubScribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubScribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      }
+      this.setState({ currentUser: userAuth });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubScribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route path="/" component={Homepage} exact />
+          <Route path="/shop" component={ShopPage} exact />
+          <Route path="/signIn" component={SignInAndSignUpPage} exact />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./sign-in-and-sign-up.styles.scss";
 import SignIn from "../../components/signIn/sign-in.component";
+import SignUp from "../../components/signUp/sign-Up.component";
 
 const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
